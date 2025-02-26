433868dcd58b31571f7a208339860d0db5cd37d2
Björn Gylling <bjorn.gylling@gmail.com>
2025-02-26 17:41:59 +0100

    refactor: make keymap easier to read

--- keyboards/hs60/v2/iso/keymaps/bjorngylling/keymap.c
+++ keyboards/hs60/v2/iso/keymaps/bjorngylling/keymap.c
@@ -40,32 +40,32 @@ enum layers { LAYER_BASE, LAYER_LOWER, LAYER_RAISE, LAYER_ADJUST };
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-[LAYER_BASE] = LAYOUT_60_iso( /* Base */
+[LAYER_BASE] = LAYOUT_60_iso(
     KC_GRV,  KC_1,    KC_2,    KC_3, KC_4, KC_5, KC_6,   KC_7, KC_8, KC_9,    KC_0,    KC_MINS,         KC_EQL,          KC_BSPC,
     KC_TAB,  KC_Q,    KC_W,    KC_E, KC_R, KC_T, KC_Y,   KC_U, KC_I, KC_O,    KC_P,    KC_LBRC,         KC_RBRC,
     QK_GESC, KC_A,    KC_S,    KC_D, KC_F, KC_G, KC_H,   KC_J, KC_K, KC_L,    KC_SCLN, KC_QUOT,         KC_NUHS,         KC_ENT,
     KC_LSFT, KC_NUBS, KC_Z,    KC_X, KC_C, KC_V, KC_B,   KC_N, KC_M, KC_COMM, KC_DOT,  KC_SLSH,                               KC_RSFT,
     KC_LCTL, KC_LALT, KC_LGUI,                               KC_SPC,                                  KC_RALT, MO(LAYER_LOWER), MO(LAYER_RAISE), KC_RCTL),
 
-[LAYER_LOWER] = LAYOUT_60_iso( /* FN */
+[LAYER_LOWER] = LAYOUT_60_iso(
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_DEL ,
-    KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, QK_BOOT,
-    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,               BR_INC,
-    KC_VOLU, KC_VOLD, KC_MUTE,                                           KC_TRNS,                                         KC_TRNS, KC_TRNS, KC_TRNS, BR_DEC),
+    _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______,  _______, _______, QK_BOOT,
+    _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______,               BR_INC,
+    _______, _______, _______,                                        _______,                                         _______, _______, _______, BR_DEC),
 
-[LAYER_RAISE] = LAYOUT_60_iso( /* Empty for dynamic keymaps */
-    KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,       TMUX_NAV_UP,   KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, TMUX_NAV_LEFT, TMUX_NAV_DOWN, TMUX_NAV_RIGHT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS, KC_TRNS,       KC_TRNS,       KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,               KC_TRNS,
-    KC_TRNS, KC_TRNS,       KC_TRNS,                                                     KC_TRNS,                                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+[LAYER_RAISE] = LAYOUT_60_iso(
+    _______, _______,       _______,       _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,       TMUX_NAV_UP,   _______,        _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, TMUX_NAV_LEFT, TMUX_NAV_DOWN, TMUX_NAV_RIGHT, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______,       _______,       _______,        _______, _______, _______, _______, _______, _______, _______, _______,               _______,
+    _______, _______,       _______,                                                     _______,                                        _______, _______, _______, _______),
 
-[LAYER_ADJUST] = LAYOUT_60_iso( /* Empty for dynamic keymaps */
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS,                 KC_TRNS,
-    KC_TRNS,  KC_TRNS,  KC_TRNS,                                            KC_TRNS,                                            KC_TRNS,  KC_TRNS,  KC_TRNS,  KC_TRNS),
+[LAYER_ADJUST] = LAYOUT_60_iso(
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,               _______,
+    _______, _______, _______,                                        _______,                                        _______, _______, _______, _______),
 };
 // clang-format on
